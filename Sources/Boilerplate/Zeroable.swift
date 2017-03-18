42fb49974112ea9cdc089354d887764a745413fd
Daniel Leping <dileping@gmail.com>
2017-03-18 20:34:14 +0200

    Zeroable

---
+++ Sources/Boilerplate/Zeroable.swift
@@ -0,0 +1,27 @@
+//===--- Zeroable.swift ------------------------------------------------------===//
+//Copyright (c) 2016 Daniel Leping (dileping)
+//
+//Licensed under the Apache License, Version 2.0 (the "License");
+//you may not use this file except in compliance with the License.
+//You may obtain a copy of the License at
+//
+//http://www.apache.org/licenses/LICENSE-2.0
+//
+//Unless required by applicable law or agreed to in writing, software
+//distributed under the License is distributed on an "AS IS" BASIS,
+//WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//See the License for the specific language governing permissions and
+//limitations under the License.
+//===----------------------------------------------------------------------===//
+
+public protocol Zeroable {
+    static var zero:Self {get}
+}
+
+extension Int : Zeroable {
+    public static let zero = 0
+}
+
+extension Int32 : Zeroable {
+    public static let zero:Int32 = 0
+}
