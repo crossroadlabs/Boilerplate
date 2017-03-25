f67fbc3d07ba1e073545f3cbc6f67f0531a0ba7d
Daniel Leping <dileping@gmail.com>
2017-03-25 15:34:18 +0200

    Added tuples

--- Sources/Boilerplate/Tuple.swift
+++ Sources/Boilerplate/Tuple.swift
@@ -14,6 +14,323 @@
 //limitations under the License.
 //===----------------------------------------------------------------------===//
 
+//======================================= TUPLE =======================================
+
+public protocol Tuple {
+    associatedtype Wrapped
+    
+    init(tuple: Wrapped)
+    init(array: [Any?])
+    
+    var tuple: Wrapped {get}
+    var stripe:[Any] {get}
+}
+
+public typealias TupleProtocol = Tuple
+
+//======================================= TUPLE1 =======================================
+
+public protocol Tuple1Protocol : Tuple {
+    associatedtype A
+}
+
+public struct Tuple1<AI> : Tuple1Protocol {
+    public typealias A = AI
+    public typealias Wrapped = (A)
+    
+    public let tuple: Wrapped
+    
+    public init(tuple: Wrapped) {
+        self.tuple = tuple
+    }
+    
+    public init(_ a: A) {
+        self.init(tuple: (a))
+    }
+    
+    public init(array: [Any?]) {
+        self.init(array.first! as! A)
+    }
+    
+    public var stripe:[Any] {
+        return [tuple]
+    }
+}
+
+//======================================= TUPLE2 =======================================
+
+public protocol Tuple2Protocol : Tuple {
+    associatedtype A
+    associatedtype B
+}
+
+public struct Tuple2<AI, BI> : Tuple2Protocol {
+    public typealias A = AI
+    public typealias B = BI
+    public typealias Wrapped = (A, B)
+    
+    public let tuple: Wrapped
+    
+    public init(tuple: Wrapped) {
+        self.tuple = tuple
+    }
+    
+    public init(_ a: A, _ b: B) {
+        self.init(tuple: (a, b))
+    }
+    
+    public init(array: [Any?]) {
+        self.init(array[0] as! A, array[1] as! B)
+    }
+    
+    public var stripe:[Any] {
+        return [tuple.0, tuple.1]
+    }
+}
+
+//======================================= TUPLE3 =======================================
+
+public struct Tuple3<A, B, C> : Tuple {
+    public typealias Wrapped = (A, B, C)
+    
+    public let tuple: Wrapped
+    
+    public init(tuple: Wrapped) {
+        self.tuple = tuple
+    }
+    
+    public init(_ a: A, _ b: B, _ c: C) {
+        self.init(tuple: (a, b, c))
+    }
+    
+    public init(array: [Any?]) {
+        self.init(array[0] as! A, array[1] as! B, array[2] as! C)
+    }
+    
+    public var stripe:[Any] {
+        return [tuple.0, tuple.1, tuple.2]
+    }
+}
+
+//======================================= TUPLE4 =======================================
+
+public struct Tuple4<A, B, C, D> : Tuple {
+    public typealias Wrapped = (A, B, C, D)
+    
+    public let tuple: Wrapped
+    
+    public init(tuple: Wrapped) {
+        self.tuple = tuple
+    }
+    
+    public init(_ a: A, _ b: B, _ c: C, _ d: D) {
+        self.init(tuple: (a, b, c, d))
+    }
+    
+    public init(array: [Any?]) {
+        self.init(array[0] as! A, array[1] as! B, array[2] as! C, array[3] as! D)
+    }
+    
+    public var stripe:[Any] {
+        return [tuple.0, tuple.1, tuple.2, tuple.3]
+    }
+}
+
+//======================================= TUPLE5 =======================================
+
+public struct Tuple5<A, B, C, D, E> : Tuple {
+    public typealias Wrapped = (A, B, C, D, E)
+    
+    public let tuple: Wrapped
+    
+    public init(tuple: Wrapped) {
+        self.tuple = tuple
+    }
+    
+    public init(_ a: A, _ b: B, _ c: C, _ d: D, _ e: E) {
+        self.init(tuple: (a, b, c, d, e))
+    }
+    
+    public init(array: [Any?]) {
+        self.init(array[0] as! A, array[1] as! B, array[2] as! C, array[3] as! D, array[4] as! E)
+    }
+    
+    public var stripe:[Any] {
+        return [tuple.0, tuple.1, tuple.2, tuple.3, tuple.4]
+    }
+}
+
+//======================================= TUPLE6 =======================================
+
+public struct Tuple6<A, B, C, D, E, F> : Tuple {
+    public typealias Wrapped = (A, B, C, D, E, F)
+    
+    public let tuple: Wrapped
+    
+    public init(tuple: Wrapped) {
+        self.tuple = tuple
+    }
+    
+    public init(_ a: A, _ b: B, _ c: C, _ d: D, _ e: E, _ f: F) {
+        self.init(tuple: (a, b, c, d, e, f))
+    }
+    
+    public init(array: [Any?]) {
+        self.init(array[0] as! A, array[1] as! B, array[2] as! C, array[3] as! D, array[4] as! E, array[5] as! F)
+    }
+    
+    public var stripe:[Any] {
+        return [tuple.0, tuple.1, tuple.2, tuple.3, tuple.4, tuple.5]
+    }
+}
+
+//======================================= TUPLE7 =======================================
+
+public struct Tuple7<A, B, C, D, E, F, G> : Tuple {
+    public typealias Wrapped = (A, B, C, D, E, F, G)
+    
+    public let tuple: Wrapped
+    
+    public init(tuple: Wrapped) {
+        self.tuple = tuple
+    }
+    
+    public init(_ a: A, _ b: B, _ c: C, _ d: D, _ e: E, _ f: F, _ g: G) {
+        self.init(tuple: (a, b, c, d, e, f, g))
+    }
+    
+    public init(array: [Any?]) {
+        self.init(array[0] as! A,
+                  array[1] as! B,
+                  array[2] as! C,
+                  array[3] as! D,
+                  array[4] as! E,
+                  array[5] as! F,
+                  array[6] as! G)
+    }
+    
+    public var stripe:[Any] {
+        return [tuple.0, tuple.1, tuple.2, tuple.3, tuple.4, tuple.5, tuple.6]
+    }
+}
+
+//======================================= TUPLE8 =======================================
+
+public struct Tuple8<A, B, C, D, E, F, G, H> : Tuple {
+    public typealias Wrapped = (A, B, C, D, E, F, G, H)
+    
+    public let tuple: Wrapped
+    
+    public init(tuple: Wrapped) {
+        self.tuple = tuple
+    }
+    
+    public init(_ a: A, _ b: B, _ c: C, _ d: D, _ e: E, _ f: F, _ g: G, _ h: H) {
+        self.init(tuple: (a, b, c, d, e, f, g, h))
+    }
+    
+    public init(array: [Any?]) {
+        self.init(array[0] as! A,
+                  array[1] as! B,
+                  array[2] as! C,
+                  array[3] as! D,
+                  array[4] as! E,
+                  array[5] as! F,
+                  array[6] as! G,
+                  array[7] as! H)
+    }
+    
+    public var stripe:[Any] {
+        return [tuple.0, tuple.1, tuple.2, tuple.3, tuple.4, tuple.5, tuple.6, tuple.7]
+    }
+}
+
+//======================================= TUPLE9 =======================================
+
+public struct Tuple9<A, B, C, D, E, F, G, H, I> : Tuple {
+    public typealias Wrapped = (A, B, C, D, E, F, G, H, I)
+    
+    public let tuple: Wrapped
+    
+    public init(tuple: Wrapped) {
+        self.tuple = tuple
+    }
+    
+    public init(_ a: A, _ b: B, _ c: C, _ d: D, _ e: E, _ f: F, _ g: G, _ h: H, _ i: I) {
+        self.init(tuple: (a, b, c, d, e, f, g, h, i))
+    }
+    
+    public init(array: [Any?]) {
+        self.init(array[0] as! A,
+                  array[1] as! B,
+                  array[2] as! C,
+                  array[3] as! D,
+                  array[4] as! E,
+                  array[5] as! F,
+                  array[6] as! G,
+                  array[7] as! H,
+                  array[8] as! I)
+    }
+    
+    public var stripe:[Any] {
+        return [tuple.0, tuple.1, tuple.2, tuple.3, tuple.4, tuple.5, tuple.6, tuple.7, tuple.8]
+    }
+}
+
+//======================================= TUPLE10 =======================================
+
+public struct Tuple10<A, B, C, D, E, F, G, H, I, J> : Tuple {
+    public typealias Wrapped = (A, B, C, D, E, F, G, H, I, J)
+    
+    public let tuple: Wrapped
+    
+    public init(tuple: Wrapped) {
+        self.tuple = tuple
+    }
+    
+    public init(_ a: A, _ b: B, _ c: C, _ d: D, _ e: E, _ f: F, _ g: G, _ h: H, _ i: I, _ j: J) {
+        self.init(tuple: (a, b, c, d, e, f, g, h, i, j))
+    }
+    
+    public init(array: [Any?]) {
+        self.init(array[0] as! A,
+                  array[1] as! B,
+                  array[2] as! C,
+                  array[3] as! D,
+                  array[4] as! E,
+                  array[5] as! F,
+                  array[6] as! G,
+                  array[7] as! H,
+                  array[8] as! I,
+                  array[9] as! J)
+    }
+    
+    public var stripe:[Any] {
+        return [tuple.0, tuple.1, tuple.2, tuple.3, tuple.4, tuple.5, tuple.6, tuple.7, tuple.8, tuple.9]
+    }
+}
+
+//======================================= CASE =======================================
+
+public protocol CaseProtocol {
+    associatedtype Tuple : TupleProtocol
+    
+    init(tuple: Tuple.Wrapped)
+    
+    var tuple: Self.Tuple.Wrapped {get}
+}
+
+public extension CaseProtocol {
+    public init(tuple: Tuple) {
+        self.init(tuple: tuple.tuple)
+    }
+    
+    public var tuple: Self.Tuple {
+        return Self.Tuple(tuple: tuple)
+    }
+}
+
+
 //======================================= FLATTEN =======================================
 
 //THREE
